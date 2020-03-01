576069688addefbaf88b3d2bf9955e8f363ccdf1
Aileen Liu <aileenliu184@gmail.com>
2020-02-29 21:27:57 -0800

    solved

---
+++ best_time_to_buy_and_sell_stock_II.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+
+        result = 0
+
+        for i in range(len(prices) - 1):
+            if prices[i + 1] > prices[i]:
+                result += prices[i + 1] - prices[i]
+
+        return result
