7adeac6bd658100b28dc309ec5270dc358b92a2b
Aileen Liu <aileenliu184@gmail.com>
2020-03-01 19:33:29 -0800

    changed reverse_a_string.py

--- reverse_a_string.py
+++ reverse_a_string.py
@@ -1,19 +1,14 @@
-# https://leetcode.com/problems/reverse-string/submissions/
+# https://leetcode.com/problems/reverse-string/
 
 def reverse_str(s):
-    # find first and last item in the list
-    left_idx, right_idx = (0, len(s) -1)
 
-    # do looping to move left and right index inwards
+    left_idx, right_idx = (0, len(s) -1)
     while (left_idx < right_idx):
-        # print(left_idx, right_idx)
         s[left_idx], s[right_idx] = s[right_idx], s[left_idx]
         left_idx += 1
         right_idx -=1
-        # print(left_idx, right_idx)
-        return s
 
-print(reverse_str(["H","a","n","n","a","h"]))
+        return s
 
 # O(n) - time complexity
 # O(1) - space complexity
