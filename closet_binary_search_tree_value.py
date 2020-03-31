6bc6a46367b4a06577b6c3e6cd15948c1bb581e9
Aileen Liu <aileenliu184@gmail.com>
2020-03-31 15:13:38 -0700

    solved closed_binary_search_tree_value.py

---
+++ closet_binary_search_tree_value.py
@@ -0,0 +1,35 @@
+# Given a non-empty binary search tree and a target value, find the value in the BST
+# that is closest to the target.
+
+# Example:
+#
+# Input: root = [4,2,5,1,3], target = 3.714286
+#
+#     4
+#    / \
+#   2   5
+#  / \
+# 1   3
+#
+# Output: 4
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution():
+    def closet_value(root, target):
+        cur = root
+        v = root.val
+
+        while root != None:
+            if abs(cur.val - target) < abs(v - target):
+                v = cur.val
+            if target < cur.val:
+                cur = cur.left
+            else:
+                cur = cur.right
+        return v
+
